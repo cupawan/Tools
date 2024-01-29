08527188f535f01194e0f251c726f496815fb98e
Pawan <pavan11896@gmail.com>
2024-01-29 11:16:00 +0530

    Initial Commit

---
+++ OpenWeatherMap/main.py
@@ -0,0 +1,228 @@
+import os
+import yaml
+import json
+import requests
+import csv
+from datetime import datetime
+import pytz
+from email_script import EmailWithPython
+import pandas as pd
+
+class OpenWeatherMap:
+    def __init__(self, config_path):
+        self.config_path = config_path
+        self.current_dir = os.path.dirname(os.path.realpath(__file__))
+        self.config = self.load_config()
+
+    def load_config(self):
+        config_path = os.path.join(self.current_dir, self.config_path)
+        try:
+            with open(config_path, 'r') as f:
+                config = yaml.safe_load(f)
+                return config
+        except Exception as e:
+            print("Error loading API_KEY",e)
+            return {"API_KEY":"NO_KEY"}
+
+    def save_csv(self, data):
+        filepath = os.path.join(self.current_dir, 'Geocoding.csv')
+        if not os.path.exists(filepath):
+            with open(filepath, 'a+') as data_file:
+                csvwriter = csv.writer(data_file)
+                csvwriter.writerows([["Address", "Latitude", "Longitude"], data])
+        else:
+            with open(filepath, 'a+') as data_file:
+                csvwriter = csv.writer(data_file)
+                csvwriter.writerow(data)
+   
+    def epcoch_to_dtobj(self,ep):
+        datetime_object_utc = datetime.utcfromtimestamp(ep)
+        target_timezone = pytz.timezone('Asia/Kolkata')
+        datetime_object_target_timezone = datetime_object_utc.replace(tzinfo=pytz.utc).astimezone(target_timezone)
+        formatted_datetime = datetime_object_target_timezone.strftime("%d %b %I:%M %p")
+        return formatted_datetime
+
+    def geocode(self, address, limit=1):
+        params = {'q': address, 'limit' : limit, 'appid' : self.config['API_KEY']}
+        response = requests.get(url=self.config["GEOCODE"],params = params).json()[0]
+        data = [address, response['lat'], response['lon']]
+        self.save_csv(data=data)
+        return response['lat'], response['lon']
+    
+    def request_weather_data(self,lat,lon, name, location):
+        params = {'lat': lat, 'lon' : lon, 'appid' : self.config['OPENWEATHERMAP_API_KEY'],'units' : 'metric'}
+        response = requests.get(url = self.config["OPENWEATHERMAP_CURRENT_WEATHER_BASE_URL"],params=params).json()
+        data = response['current']
+        cols = ["Data & Time", "Sunrise", "Sunset", "Tempreture", "Feels Like", "Pressure", "Humidity", "Dew Point", "UV Index", "Clouds", "Visibility", "Wind Speed", "Wind Degree"]
+        vals = [str(self.epcoch_to_dtobj(data['dt'])),str(self.epcoch_to_dtobj(data['sunrise'])),str(self.epcoch_to_dtobj(data['sunset'])),f"{data['temp']}°C",f"{data['feels_like']}°C",f"{data['pressure']}mb",f"{data['humidity']}%",f"{data['dew_point']}°C",str(data["uvi"]),f"{data['clouds']}%",f"{data['visibility']} Metres",f"{data['wind_speed']}m/s",str(data['wind_deg'])]
+        email_body = f"""
+                    <html>
+                    <head>
+                        <style>
+                            body {{
+                                font-family: 'Arial', sans-serif;
+                                background-color: #f4f4f4;
+                                color: #333;
+                            }}
+                            table {{
+                                width: 80%;
+                                margin: 20px auto;
+                                border-collapse: collapse;
+                                border: 1px solid #ddd;
+                            }}
+                            th, td {{
+                                padding: 12px;
+                                text-align: left;
+                                border-bottom: 1px solid #ddd;
+                            }}
+                            th {{
+                                background-color: #f2f2f2;
+                            }}
+                        </style>
+                    </head>
+                    <body>
+
+                    <h2>Weather Information</h2>
+
+                    <table>
+                        <tr>
+                            <th>Data & Time</th>
+                            <td>{vals[0]}</td>
+                        </tr>
+                        <tr>
+                            <th>Sunrise</th>
+                            <td>{vals[1]}</td>
+                        </tr>
+                        <tr>
+                            <th>Sunset</th>
+                            <td>{vals[2]}</td>
+                        </tr>
+                        <tr>
+                            <th>Temperature</th>
+                            <td>{vals[3]}</td>
+                        </tr>
+                        <tr>
+                            <th>Feels Like</th>
+                            <td>{vals[4]}</td>
+                        </tr>
+                        <tr>
+                            <th>Pressure</th>
+                            <td>{vals[5]}</td>
+                        </tr>
+                        <tr>
+                            <th>Humidity</th>
+                            <td>{vals[6]}</td>
+                        </tr>
+                        <tr>
+                            <th>Dew Point</th>
+                            <td>{vals[7]}</td>
+                        </tr>
+                        <tr>
+                            <th>UV Index</th>
+                            <td>{vals[8]}</td>
+                        </tr>
+                        <tr>
+                            <th>Clouds</th>
+                            <td>{vals[9]}</td>
+                        </tr>
+                        <tr>
+                            <th>Visibility</th>
+                            <td>{vals[10]}</td>
+                        </tr>
+                        <tr>
+                            <th>Wind Speed</th>
+                            <td>{vals[11]}</td>
+                        </tr>
+                        <tr>
+                            <th>Wind Degree</th>
+                            <td>{vals[12]}</td>
+                        </tr>
+                    </table>
+
+                    </body>
+                    </html>
+                    """
+        weather_report = f"""
+                            <html>
+                            <head>
+                                <style>
+                                    body {{
+                                        font-family: 'Arial', sans-serif;
+                                        background-color: #eff8ff;
+                                        color: #333;
+                                        margin: 20px;
+                                    }}
+                                    .report-container {{
+                                        max-width: 600px;
+                                        margin: 0 auto;
+                                        padding: 20px;
+                                        border: 2px solid #3498db;
+                                        border-radius: 10px;
+                                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                                        background-color: #fff;
+                                    }}
+                                    .highlight {{
+                                        color: #e74c3c;
+                                        font-weight: bold;
+                                    }}
+                                    .important {{
+                                        background-color: #f8d7da;
+                                        padding: 2px;
+                                        color: #721c24;
+                                        border-radius: 3px;
+                                    }}
+                                </style>
+                            </head>
+                            <body>
+
+                            <div class="report-container">
+                                <h2>Weather Report</h2>
+                                <h3>Good Morning, {name}</h3>
+
+                                <p>
+                                    The current weather report for <span class="highlight">{vals[0]}</span> at  <span class="highlight">{location}</span> is as follows:
+                                </p>
+
+                                <p>
+                                    <strong>Sunrise:</strong> {vals[1]} | <strong>Sunset:</strong> {vals[2]}
+                                </p>
+
+                                <p>
+                                    The temperature is <span class="highlight">{vals[3]}</span>, and it feels like <span class="highlight">{vals[4]}</span>. The atmospheric pressure is <span class="highlight">{vals[5]}</span>,
+                                    humidity is <span class="highlight">{vals[6]}</span>, and the dew point is <span class="highlight">{vals[7]}</span>.
+                                </p>
+
+                                <p>
+                                    The UV Index is <span class="highlight">{vals[8]}</span>, clouds cover <span class="highlight">{vals[9]}</span> of the sky, and visibility is <span class="highlight">{vals[10]}</span>.
+                                </p>
+
+                                <p>
+                                    Wind speed is <span class="highlight">{vals[11]}</span>, coming from <span class="highlight">{vals[12]}</span> degrees.
+                                </p>
+                                <p> Have a nice day! </p>
+                            </div>
+
+                            </body>
+                            </html>
+                            """
+ 
+        return weather_report
+      
+
+if __name__ == "__main__":
+    config_file_path = os.environ.get("CONFIG_PATH")
+    if not config_file_path:
+        print("CONFIG FILE PATH NOT SET")
+    contacts = {'username@gmail.com' :[('latitude_value', 'longitude_value'),'location','name'], 'username2@gmail.com' :[('latitude_value2', 'longitude_value2'),'location2', 'name2']}
+    try:
+        owm = OpenWeatherMap(config_path= config_file_path)
+        gc = EmailWithPython(config_path =config_file_path, is_html = True)
+        for key,value in contacts.items():
+            rec_mail = key
+            name = value[-1]
+            location,latitude,longitude  = value[1],value[0][0],value[0][1] 
+            body = owm.request_weather_data(lat = latitude, lon=longitude, name = name, location=location)
+            gc.send_email(rec_email=rec_mail,subject="Today's Weather Report",body = body)
+            print("Email Sent to {}".format(name))
+    except Exception as e:
+        print("There was an error: {}".format(e))
